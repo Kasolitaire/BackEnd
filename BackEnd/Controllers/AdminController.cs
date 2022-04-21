37728fbf4bb0c3a187cfa65ebf14450fa52d252d
Kasolitaire <90893226+Kasolitaire@users.noreply.github.com>
2022-04-21 13:34:01 +0300

    AdminController can now update a user

--- BackEnd/Controllers/AdminController.cs
+++ BackEnd/Controllers/AdminController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using DatabaseHandler;
+using System.Linq;
+using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -9,11 +12,26 @@ namespace BackEnd.Controllers
     public class AdminController : ControllerBase
     {
 
-        // GET Admin>/
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET Admin>/<TableYouWant>
+        [HttpGet("{requestedTable}")]
+        public IActionResult GetRequestedTable(string requestedTable)
         {
-            return "value";
+            requestedTable = requestedTable.ToUpper();
+            CarRentalDatabaseContext dbInteraction = new CarRentalDatabaseContext();
+            // Maybe add NoContent if the table is empty
+            switch (requestedTable)
+            {
+                case "USERS":
+                    return Ok(dbInteraction.Users);
+                case "RENTALORDERDETAILS":
+                    return Ok(dbInteraction.RentalOrderDetails);
+                case "VEHICLETYPE":
+                    return Ok(dbInteraction.VehicleTypes);
+                case "VEHICLEINVENTORY":
+                    return Ok(dbInteraction.VehicleInventories);
+                default:
+                    return BadRequest();
+            }
         }
 
         // POST api/<AdminController>
@@ -22,14 +40,34 @@ namespace BackEnd.Controllers
         {
         }
 
-        // PUT api/<AdminController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        ///Admin/UpdateUser/<UserID>
+        [HttpPut("[action]/{UserID}")]
+        public IActionResult UpdateUser(int UserID, [FromBody] User updatedUser)
         {
+            //context.Entry(temp).CurrentValues.SetValues(order);
+            //context.SaveChanges();
+            using (CarRentalDatabaseContext dbInteraction = new CarRentalDatabaseContext()) 
+            {
+                User originalUser = dbInteraction.Users.FirstOrDefault(user => user.UserId == UserID);
+                if (originalUser != null) 
+                {
+                    updatedUser.UserId = originalUser.UserId;
+                    dbInteraction.Entry(originalUser).CurrentValues.SetValues(updatedUser);
+                    dbInteraction.SaveChanges();
+                    return Ok(updatedUser);
+                }
+                else
+                {
+                    return NoContent();
+                }
+            }
         }
 
+
+
+
         // DELETE api/<AdminController>/5
-        [HttpDelete("{id}")]
+        [HttpDelete("{id}", Name = "bob")]
         public void Delete(int id)
         {
         }
